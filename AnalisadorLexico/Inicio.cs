9822ecc174371d49ee5b072712be300df093bcd8
Davi Nascimento <davi-ds15@hotmail.com>
2022-03-26 16:52:07 -0300

    Update Inicio.cs
    Implementação

--- AnalisadorLexico/Inicio.cs
+++ AnalisadorLexico/Inicio.cs
@@ -2,16 +2,15 @@ namespace AnalisadorLexico
 {
     public partial class Inicio : Form
     {
-        char[] simbolos = { '(', ')', '{', '}', ',', ';', '[', ']' };
+        char[] simbolos = { '(', ')', '{', '}', ',', ';', '[', ']'};
         string[] keys = { "main", "void", "int", "while", "for", "then", "if", "else", "float", "double", "string", "char", "boolean", "bool", "break", "switch", "case", "do" };
-        char[] caractere = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        char[] caractere = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','_','.'};
         string[] operador = { "+", "-", "*", "/", ">", "<", "=", "==", "!=", ">=", "<=", "&&", "||" };
+        char[] numero = { '0','1', '2', '3', '4', '5', '6', '7', '8', '9'};
         public Inicio()
         {
             InitializeComponent();
         }
-
-
         bool Simbolo(char c)
         {
             foreach (char ch in simbolos)
@@ -34,7 +33,6 @@ namespace AnalisadorLexico
             }
             return false;
         }
-
         bool Keys(string s)
         {
             foreach (string ch in keys)
@@ -46,7 +44,6 @@ namespace AnalisadorLexico
             }
             return false;
         }
-
         bool Caractere(char c)
         {
             foreach (char ch in caractere)
@@ -58,53 +55,107 @@ namespace AnalisadorLexico
             }
             return false;
         }
-
+        bool Numero(char c)
+        {
+            foreach (char ch in numero)
+            {
+                if (ch == c)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
         void Executa() {
             Dictionary<string,string> token = new Dictionary<string, string>();
-            string Aux = R_text.Text;
+            string Aux = R_text.Text+" ";
+            int i = 0;
             string Aux1 = "";
+            char cenario=' ';
             foreach (char item in Aux)
             {
                 if(item!=' ')
                 {
+                    if (cenario=='.')
+                    {
+                        if (Numero(item))
+                        {
+                            Aux1 += cenario;
+                            cenario = ' ';
+                        }
+                        else if(Caractere(item))
+                        {
+                            token.Add((i++).ToString(), Aux1.ToString());
+                            token.Add((i++).ToString(), cenario.ToString());
+                            cenario= ' ';
+                            Aux1 =" ";
+                        }
+                    }
                     if (Simbolo(item))
                     {
-                        Aux1 += item.ToString();
+                        if (Aux1 == "")
+                        {
+                            token.Add((i++).ToString(), item.ToString());
+                            Aux1 = "";                            
+                        }
+                        else if(Aux1!=""){
+                             token.Add((i++).ToString(), Aux1);
+                             Aux1 = "";
+                             token.Add((i++).ToString(), item.ToString());
+
+                        }
                     }else if (Operador(item))
                     {
-                        Aux1 += item.ToString();
+                        if (Aux1 == "")
+                        {
+                            token.Add((i++).ToString(), item.ToString());
+                            Aux1 = "";
+                        }
+                        else if (Aux1 != "")
+                        {
+                            token.Add((i++).ToString(), Aux1);
+                            Aux1 = "";
+                            token.Add((i++).ToString(), item.ToString());
+                        }
                     }
-                    else if (Keys(item.ToString()))
+                    else if(Caractere(item))
                     {
-                        Aux1 += item.ToString();
+                        if (item == '.')
+                            cenario = item;
+                        else
+                            Aux1 += item.ToString();
+                        if (Keys(Aux1))
+                        {
+                            token.Add("Key"+(i++), Aux1);
+                            Aux1 = "";
+                        }
                     }
-                    else
+                    else if (Numero(item))
                     {
+                        
                         Aux1 += item.ToString();
                     }
                 }
                 else
                 {
-                    if (Aux1 != "")
+                    if (Aux1 !="")
                     {
-                        token.Add(Aux1, Aux1);
+                        token.Add((i++).ToString(), Aux1);
                         Aux1 = "";
                     }
                 }
             }
-            foreach (string key in token.Keys)
+            foreach (var item in token)
             {
-                R_Text1.Text += token[key] +"\n";
+                R_Text1.Text += item.Key + " " + item.Value +"\n" ;
             }
         }
-
         private void txt_InserirCodigo_Click(object sender, EventArgs e)
         {
             R_text.Enabled= true;
             R_text.Visible = true;
             Executa();
         }
-
         private void txt_LerCodigo_Click(object sender, EventArgs e)
         {
 
