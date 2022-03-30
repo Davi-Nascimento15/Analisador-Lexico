b43a6e6b352790a8a4415c394d4ab65b65654d50
Davi Nascimento <davi-ds15@hotmail.com>
2022-03-30 20:33:37 -0300

    Update Inicio.cs
    Update

--- AnalisadorLexico/Inicio.cs
+++ AnalisadorLexico/Inicio.cs
@@ -2,11 +2,11 @@ namespace AnalisadorLexico
 {
     public partial class Inicio : Form
     {
-        char[] simbolos = { '(', ')', '{', '}', ',', ';', '[', ']'};
+        char[] simbolos = { '(', ')', '{', '}', ',', ';', '[', ']' };
         string[] keys = { "main", "void", "int", "while", "for", "then", "if", "else", "float", "double", "string", "char", "boolean", "bool", "break", "switch", "case", "do" };
-        char[] caractere = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','_','.'};
+        char[] caractere = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_', '.' };
         string[] operador = { "+", "-", "*", "/", ">", "<", "=", "==", "!=", ">=", "<=", "&&", "||" };
-        char[] numero = { '0','1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        char[] numero = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         public Inicio()
         {
             InitializeComponent();
@@ -15,7 +15,7 @@ namespace AnalisadorLexico
         {
             foreach (char ch in simbolos)
             {
-                if (ch== c)
+                if (ch == c)
                 {
                     return true;
                 }
@@ -66,59 +66,88 @@ namespace AnalisadorLexico
             }
             return false;
         }
-        void Executa() {
-            Dictionary<string,string> token = new Dictionary<string, string>();
-            string Aux = R_text.Text+" ";
+        void Executa()
+        {
+
+            Dictionary<string, string> token = new Dictionary<string, string>();
+            string Aux = R_text.Text + " ";
             int i = 0;
             string Aux1 = "";
-            char cenario=' ';
+            char cenario = ' ';
+            string AuxNumero = "";
+            string AuxCaractere = "";
+
             foreach (char item in Aux)
             {
-                if(item!=' ')
-                {
-                    if (cenario=='.')
+                //if (item != ' ')
+                //{ 
+                    if (cenario == '.')
                     {
                         if (Numero(item))
                         {
                             Aux1 += cenario;
-                            cenario = ' ';
+                            cenario = ' ';///numero float
                         }
-                        else if(Caractere(item))
+                        else if (Caractere(item))
                         {
-                            token.Add((i++).ToString(), Aux1.ToString());
-                            token.Add((i++).ToString(), cenario.ToString());
-                            cenario= ' ';
-                            Aux1 =" ";
+                            token.Add("ID" + (i++).ToString(), Aux1.ToString());
+                            token.Add((i++).ToString(), cenario.ToString());///ponto de função
+                            cenario = ' ';
+                            Aux1 = " ";
                         }
+                    }
+                    if (Operador(cenario))
+                    {
+                        if (Operador(item))
+                        {
+                            token.Add("Oper" + (i++).ToString(), cenario.ToString() + item.ToString());
+                            cenario = ' ';
+                            Aux1 = "";
+                        }
+                        else
+                        {
+                            token.Add("Oper" + (i++).ToString(), cenario.ToString());
+                            cenario = ' ';
+                            Aux1 = "";
+                        }
+                    }
+                    if (Numero(cenario))
+                    {
+                        AuxNumero += cenario.ToString();
+                        //cenario = ' ';  
                     }
                     if (Simbolo(item))
                     {
                         if (Aux1 == "")
                         {
-                            token.Add((i++).ToString(), item.ToString());
-                            Aux1 = "";                            
-                        }
-                        else if(Aux1!=""){
-                             token.Add((i++).ToString(), Aux1);
-                             Aux1 = "";
-                             token.Add((i++).ToString(), item.ToString());
-
-                        }
-                    }else if (Operador(item))
-                    {
-                        if (Aux1 == "")
-                        {
-                            token.Add((i++).ToString(), item.ToString());
-                            Aux1 = "";
+                            token.Add("simbol" + (i++).ToString(), item.ToString());
+                            Aux1 = "";//simbolo                            
                         }
                         else if (Aux1 != "")
                         {
                             token.Add((i++).ToString(), Aux1);
                             Aux1 = "";
                             token.Add((i++).ToString(), item.ToString());
+
                         }
                     }
-                    else if(Caractere(item))
+                    else if (Operador(item))
+                    {
+                        if (Aux1 == "")
+                        {
+                            // token.Add("oper"+(i++).ToString(), item.ToString());
+                            //Aux1 = "";
+                            if (cenario == ' ')
+                                cenario = item;
+                        }
+                        //else if (Aux1 != "")
+                        //{
+                        //    token.Add((i++).ToString(), Aux1);
+                        //    Aux1 = "";
+                        //    token.Add((i++).ToString(), item.ToString());
+                        //}
+                    }
+                    else if (Caractere(item))
                     {
                         if (item == '.')
                             cenario = item;
@@ -126,33 +155,39 @@ namespace AnalisadorLexico
                             Aux1 += item.ToString();
                         if (Keys(Aux1))
                         {
-                            token.Add("Key"+(i++), Aux1);
+                            token.Add("Key" + (i++), Aux1);
                             Aux1 = "";
                         }
                     }
                     else if (Numero(item))
                     {
-                        
-                        Aux1 += item.ToString();
+                        if (cenario == ' ' || Numero(cenario))
+                            cenario = item;
                     }
-                }
-                else
-                {
-                    if (Aux1 !="")
+                    if ((AuxNumero != "") && (!Numero(item)))
                     {
-                        token.Add((i++).ToString(), Aux1);
-                        Aux1 = "";
+                        token.Add("numero" + (i++).ToString(), AuxNumero);
+                        AuxNumero = "";
+                        cenario = ' ';
                     }
-                }
+                    //else
+                    //{
+                   //     if (Aux1 != "")
+                   //     {
+                   //         token.Add((i++).ToString(), Aux1);
+                   //         Aux1 = "";
+                   //     }
+                   //}
+                //}
             }
             foreach (var item in token)
             {
-                R_Text1.Text += item.Key + " " + item.Value +"\n" ;
+                R_Text1.Text += item.Key + " " + item.Value + "\n";
             }
         }
         private void txt_InserirCodigo_Click(object sender, EventArgs e)
         {
-            R_text.Enabled= true;
+            R_text.Enabled = true;
             R_text.Visible = true;
             Executa();
         }
